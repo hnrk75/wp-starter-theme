ea70c8c92d106bdaa0dcb000388ebc7caa3ebdf5
Henrik Pettersson <henrik.pettersson@knowit.se>
2024-04-04 07:54:35 +0200

    update composer, gulp, package files

--- web/app/themes/starterwp-bs5/gulpfile.js
+++ web/app/themes/starterwp-bs5/gulpfile.js
@@ -1,55 +1,47 @@
-// Load all the modules from package.json
-var gulp       = require("gulp"),
-  plumber      = require("gulp-plumber"),
-  autoprefixer = require("gulp-autoprefixer"),
-  watch        = require("gulp-watch"),
-  jshint       = require("gulp-jshint"),
-  stylish      = require("jshint-stylish"),
-  uglify       = require("gulp-uglify"),
-  rename       = require("gulp-rename"),
-  notify       = require("gulp-notify"),
-  include      = require("gulp-include"),
-  sass         = require("gulp-sass")(require("sass"));
-  browserSync  = require("browser-sync").create(),
-  critical     = require("critical"),
-  zip          = require("gulp-zip");
+import gulp from "gulp";
+import plumber from "gulp-plumber";
+import autoprefixer from "gulp-autoprefixer";
+import watch from "gulp-watch";
+import jshint from "gulp-jshint";
+import stylish from "jshint-stylish";
+import uglify from "gulp-uglify";
+import rename from "gulp-rename";
+import notify from "gulp-notify";
+import include from "gulp-include";
+import sass from "gulp-sass";
+import browserSync from "browser-sync";
+import critical from "critical";
+import zip from "gulp-zip";
 
-var config = {
+const config = {
   nodeDir: "./node_modules"
 };
 
-// Default error handler
-var onError = function (err) {
+const onError = function (err) {
   console.log("An error occured:", err.message);
   this.emit("end");
 };
 
-// JS to watch
-var jsFiles = ["./js/**/*.js", "!./js/dist/*.js"];
+const jsFiles = ["./js/**/*.js", "!./js/dist/*.js"];
+const cssFiles = ["./sass/**/*.scss"];
 
-// Sass files to watch
-var cssFiles = ["./sass/**/*.scss"];
+const browserSyncWatchFiles = ["./*.min.css", "./js/**/*.min.js", "./**/*.php"];
 
-// automatically reloads the page when files changed
-var browserSyncWatchFiles = ["./*.min.css", "./js/**/*.min.js", "./**/*.php"];
-
-// see: https://www.browsersync.io/docs/options/
-var browserSyncOptions = {
+const browserSyncOptions = {
   watchTask: true,
   proxy: "http://starterwp.local/"
 };
 
-// Zip files up
 gulp.task("zip", function () {
   return gulp
     .src(
       [
-		"**/*",
-		"!node_modules/**",
-		"!starterwp.zip",
-		"!gulpfile.js",
-		"!package-lock.json",
-		"!yarn.lock"
+        "**/*",
+        "!node_modules/**",
+        "!starterwp.zip",
+        "!gulpfile.js",
+        "!package-lock.json",
+        "!yarn.lock"
       ],
       { base: "." }
     )
@@ -57,8 +49,6 @@ gulp.task("zip", function () {
     .pipe(gulp.dest("."));
 });
 
-// Jshint outputs any kind of javascript problems you might have
-// Only checks javascript files inside /src directory
 gulp.task("jshint", function () {
   return gulp
     .src("./js/src/*.js")
@@ -67,9 +57,6 @@ gulp.task("jshint", function () {
     .pipe(jshint.reporter("fail"));
 });
 
-// Concatenates all files that it finds in the manifest
-// and creates two versions: normal and minified.
-// It's dependent on the jshint task to succeed.
 gulp.task("scripts", function () {
   return (
     gulp
@@ -77,8 +64,6 @@ gulp.task("scripts", function () {
       .pipe(include())
       .pipe(rename({ basename: "scripts" }))
       .pipe(gulp.dest("./js/dist"))
-      // Normal done, time to create the minified javascript (scripts.min.js)
-      // remove the following 3 lines if you don't want it
       .pipe(uglify())
       .pipe(rename({ suffix: ".min" }))
       .pipe(gulp.dest("./js/dist"))
@@ -87,7 +72,6 @@ gulp.task("scripts", function () {
   );
 });
 
-// Sass - Creates a regular and minified .css file in root
 gulp.task("sass", function () {
   return gulp
     .src("./sass/style.scss")
@@ -97,7 +81,6 @@ gulp.task("sass", function () {
         errLogToConsole: true,
         precision: 8,
         noCache: true,
-        //imagePath: 'assets/img',
         includePaths: [config.nodeDir + "/bootstrap/scss"]
       }).on("error", sass.logError)
     )
@@ -110,7 +93,6 @@ gulp.task("sass", function () {
     .pipe(notify({ title: "Sass", message: "sass task complete" }));
 });
 
-// Generate & Inline Critical-path CSS
 gulp.task("critical", function (cb) {
   critical.generate({
     base: "./",
@@ -135,17 +117,12 @@ gulp.task("critical", function (cb) {
   });
 });
 
-// Starts browser-sync task for starting the server.
 gulp.task("browser-sync", function () {
   browserSync.init(browserSyncWatchFiles, browserSyncOptions);
 });
 
-// Start the livereload server and watch files for change
 gulp.task("watch", function () {
-  // don't listen to whole js folder, it'll create an infinite loop
-  // run jshint be compiling js code
   gulp.watch(jsFiles, gulp.series("jshint", gulp.parallel("scripts")));
-
   gulp.watch(cssFiles, gulp.parallel("sass"));
 });
 
