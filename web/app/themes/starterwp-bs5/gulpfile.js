0a9e838e663c289f892eb216772edb0ce6893805
Henrik Pettersson <henrik.pettersson@knowit.se>
2024-05-16 14:52:37 +0200

    fix sass compiler bugg

--- web/app/themes/starterwp-bs5/gulpfile.js
+++ web/app/themes/starterwp-bs5/gulpfile.js
@@ -1,129 +1,152 @@
-import gulp from "gulp";
-import plumber from "gulp-plumber";
-import autoprefixer from "gulp-autoprefixer";
-import watch from "gulp-watch";
-import jshint from "gulp-jshint";
-import stylish from "jshint-stylish";
-import uglify from "gulp-uglify";
-import rename from "gulp-rename";
-import notify from "gulp-notify";
-import include from "gulp-include";
-import sass from "gulp-sass";
-import browserSync from "browser-sync";
-import critical from "critical";
-import zip from "gulp-zip";
+// Load all the modules from package.json
+var gulp       = require("gulp"),
+plumber      = require("gulp-plumber"),
+autoprefixer = require("gulp-autoprefixer"),
+watch        = require("gulp-watch"),
+jshint       = require("gulp-jshint"),
+stylish      = require("jshint-stylish"),
+uglify       = require("gulp-uglify"),
+rename       = require("gulp-rename"),
+notify       = require("gulp-notify"),
+include      = require("gulp-include"),
+sass         = require("gulp-sass")(require("sass"));
+browserSync  = require("browser-sync").create(),
+critical     = require("critical"),
+zip          = require("gulp-zip");
 
-const config = {
-  nodeDir: "./node_modules"
+var config = {
+nodeDir: "./node_modules"
 };
 
-const onError = function (err) {
-  console.log("An error occured:", err.message);
-  this.emit("end");
+// Default error handler
+var onError = function (err) {
+console.log("An error occured:", err.message);
+this.emit("end");
 };
 
-const jsFiles = ["./js/**/*.js", "!./js/dist/*.js"];
-const cssFiles = ["./sass/**/*.scss"];
+// JS to watch
+var jsFiles = ["./js/**/*.js", "!./js/dist/*.js"];
 
-const browserSyncWatchFiles = ["./*.min.css", "./js/**/*.min.js", "./**/*.php"];
+// Sass files to watch
+var cssFiles = ["./sass/**/*.scss"];
 
-const browserSyncOptions = {
-  watchTask: true,
-  proxy: "http://starterwp.local/"
+// automatically reloads the page when files changed
+var browserSyncWatchFiles = ["./*.min.css", "./js/**/*.min.js", "./**/*.php"];
+
+// see: https://www.browsersync.io/docs/options/
+var browserSyncOptions = {
+watchTask: true,
+proxy: "http://wp-starter-theme-bs5.local/"
 };
 
+// Zip files up
 gulp.task("zip", function () {
-  return gulp
-    .src(
-      [
-        "**/*",
-        "!node_modules/**",
-        "!starterwp.zip",
-        "!gulpfile.js",
-        "!package-lock.json",
-        "!yarn.lock"
-      ],
-      { base: "." }
-    )
-    .pipe(zip("starterwp-bs5.zip"))
-    .pipe(gulp.dest("."));
+return gulp
+	.src(
+	[
+		"**/*",
+		"!node_modules/**",
+		"!starterwp-bs5.zip",
+		"!gulpfile.js",
+		"!package-lock.json",
+		"!yarn.lock"
+	],
+	{ base: "." }
+	)
+	.pipe(zip("starterwp-bs5.zip"))
+	.pipe(gulp.dest("."));
 });
 
+// Jshint outputs any kind of javascript problems you might have
+// Only checks javascript files inside /src directory
 gulp.task("jshint", function () {
-  return gulp
-    .src("./js/src/*.js")
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish))
-    .pipe(jshint.reporter("fail"));
+return gulp
+	.src("./js/src/*.js")
+	.pipe(jshint())
+	.pipe(jshint.reporter(stylish))
+	.pipe(jshint.reporter("fail"));
 });
 
+// Concatenates all files that it finds in the manifest
+// and creates two versions: normal and minified.
+// It's dependent on the jshint task to succeed.
 gulp.task("scripts", function () {
-  return (
-    gulp
-      .src("./js/manifest.js")
-      .pipe(include())
-      .pipe(rename({ basename: "scripts" }))
-      .pipe(gulp.dest("./js/dist"))
-      .pipe(uglify())
-      .pipe(rename({ suffix: ".min" }))
-      .pipe(gulp.dest("./js/dist"))
-      .pipe(browserSync.reload({ stream: true }))
-      .pipe(notify({ message: "scripts task complete" }))
-  );
+return (
+	gulp
+	.src("./js/manifest.js")
+	.pipe(include())
+	.pipe(rename({ basename: "scripts" }))
+	.pipe(gulp.dest("./js/dist"))
+	// Normal done, time to create the minified javascript (scripts.min.js)
+	// remove the following 3 lines if you don't want it
+	.pipe(uglify())
+	.pipe(rename({ suffix: ".min" }))
+	.pipe(gulp.dest("./js/dist"))
+	.pipe(browserSync.reload({ stream: true }))
+	.pipe(notify({ message: "scripts task complete" }))
+);
 });
 
+// Sass - Creates a regular and minified .css file in root
 gulp.task("sass", function () {
-  return gulp
-    .src("./sass/style.scss")
-    .pipe(plumber())
-    .pipe(
-      sass({
-        errLogToConsole: true,
-        precision: 8,
-        noCache: true,
-        includePaths: [config.nodeDir + "/bootstrap/scss"]
-      }).on("error", sass.logError)
-    )
-    .pipe(autoprefixer())
-    .pipe(gulp.dest("."))
-    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(rename({ suffix: ".min" }))
-    .pipe(gulp.dest("."))
-    .pipe(browserSync.reload({ stream: true }))
-    .pipe(notify({ title: "Sass", message: "sass task complete" }));
+return gulp
+	.src("./sass/style.scss")
+	.pipe(plumber())
+	.pipe(
+	sass({
+		errLogToConsole: true,
+		precision: 8,
+		noCache: true,
+		//imagePath: 'assets/img',
+		includePaths: [config.nodeDir + "/bootstrap/scss"]
+	}).on("error", sass.logError)
+	)
+	.pipe(autoprefixer())
+	.pipe(gulp.dest("."))
+	.pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+	.pipe(rename({ suffix: ".min" }))
+	.pipe(gulp.dest("."))
+	.pipe(browserSync.reload({ stream: true }))
+	.pipe(notify({ title: "Sass", message: "sass task complete" }));
 });
 
+// Generate & Inline Critical-path CSS
 gulp.task("critical", function (cb) {
-  critical.generate({
-    base: "./",
-    src: "http://starterwp.local/",
-    dest: "css/home.min.css",
-    ignore: ["@font-face"],
-    dimensions: [
-      {
-        width: 320,
-        height: 480
-      },
-      {
-        width: 768,
-        height: 1024
-      },
-      {
-        width: 1280,
-        height: 960
-      }
-    ],
-    minify: true
-  });
+critical.generate({
+	base: "./",
+	src: "http://wp-starter-theme-bs5.local/",
+	dest: "css/home.min.css",
+	ignore: ["@font-face"],
+	dimensions: [
+	{
+		width: 320,
+		height: 480
+	},
+	{
+		width: 768,
+		height: 1024
+	},
+	{
+		width: 1280,
+		height: 960
+	}
+	],
+	minify: true
+});
 });
 
+// Starts browser-sync task for starting the server.
 gulp.task("browser-sync", function () {
-  browserSync.init(browserSyncWatchFiles, browserSyncOptions);
+browserSync.init(browserSyncWatchFiles, browserSyncOptions);
 });
 
+// Start the livereload server and watch files for change
 gulp.task("watch", function () {
-  gulp.watch(jsFiles, gulp.series("jshint", gulp.parallel("scripts")));
-  gulp.watch(cssFiles, gulp.parallel("sass"));
+// don't listen to whole js folder, it'll create an infinite loop
+// run jshint be compiling js code
+gulp.watch(jsFiles, gulp.series("jshint", gulp.parallel("scripts")));
+
+gulp.watch(cssFiles, gulp.parallel("sass"));
 });
 
 gulp.task("default", gulp.parallel("watch", "browser-sync"));
